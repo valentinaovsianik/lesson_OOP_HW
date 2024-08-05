704c08cdce8a2b8c3cffcb3e418b41928ff96d20
Valentina Ovsianik <forovp@gmail.com>
2024-08-05 23:16:10 +0300

    Классы Product и Category перенесены в отдельные модули

---
+++ src/category.py
@@ -0,0 +1,34 @@
+from src.product import Product
+
+class Category:
+    """Класс категории продуктов"""
+
+    name: str
+    description: str
+    products: list[dict]
+
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.__products = products  # Приватный атрибут для хранения продуктов
+        Category.category_count += 1  # Увеличиваем счетчик категорий при создании нового объекта
+
+    def add_product(self, product):
+        """Добавляет продукт в категорию"""
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += product.quantity  # Увеличиваем общий счетчик на количество добавленного продукта
+        else:
+            raise TypeError("Можно добавлять только экземпляры класса Product или его наследников")
+
+    @property
+    def products(self):
+        """Возвращает список продуктов в виде строки"""
+        return "\n".join(str(product) for product in self.__products)
+
+    def __str__(self):
+        total_quantity = sum(product.quantity for product in self.__products)
+        return f"{self.name}, количество продуктов: {total_quantity} шт."
---
+++ src/product.py
@@ -0,0 +1,46 @@
+class Product:
+    """Класс продуктов"""
+
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.__price = price  # Приватный атрибут для цены
+        self.quantity = quantity
+
+    @property
+    def price(self):
+        """Геттер для цены"""
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        """Сеттер для цены"""
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = value
+
+    @classmethod
+    def new_product(cls, product_info):
+        """Создает новый объект Product на основе словаря с параметрами"""
+        name = product_info.get("name")
+        description = product_info.get("description")
+        price = product_info.get("price")
+        quantity = product_info.get("quantity")
+        return cls(name, description, price, quantity)
+
+    def __str__(self):
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        if isinstance(other, Product):
+            if type(self) == type(other):
+                return (self.price * self.quantity) + (other.price * other.quantity)
+            else:
+                raise TypeError("Нельзя складывать продукты разных классов")
+        return NotImplemented
